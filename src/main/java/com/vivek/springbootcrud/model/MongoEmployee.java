221e1aaf5336dd21e4d5ce009f5e211778e1d6a0
VivekDreamer <vivekmakv@gmail.com>
2023-08-04 23:48:40 +0530

    doing changes to implement mongo crud operation in spring boot

--- src/main/java/com/vivek/springbootcrud/model/MongoEmployee.java
+++ src/main/java/com/vivek/springbootcrud/model/MongoEmployee.java
@@ -1,5 +1,8 @@
 package com.vivek.springbootcrud.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +10,9 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor 
+@Document(collection = "MongoEmployee")
 public class MongoEmployee {
+    @Id
     private int id;
     private String name;
     private String address;
