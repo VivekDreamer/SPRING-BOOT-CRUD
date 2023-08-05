91acfa3f2a5a4027cdba258f75b61d4c9c119fb6
VivekDreamer <vivekmakv@gmail.com>
2023-08-05 23:30:42 +0530

    id type is changed to ObjectId from int

--- src/main/java/com/vivek/springbootcrud/model/MongoEmployee.java
+++ src/main/java/com/vivek/springbootcrud/model/MongoEmployee.java
@@ -1,8 +1,9 @@
 package com.vivek.springbootcrud.model;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import lombok.NoArgsConstructor;
 @Document(collection = "MongoEmployee")
 public class MongoEmployee {
     @Id
-    private int id;
+    private ObjectId id;
     private String name;
     private String address;
 }
