9d4c9605dd66fc3439daf7c3e5375119e72fd364
VivekDreamer <vivekmakv@gmail.com>
2023-08-05 23:30:02 +0530

    save api added in Controller using mongo

--- src/main/java/com/vivek/springbootcrud/controller/MongoEmpController.java
+++ src/main/java/com/vivek/springbootcrud/controller/MongoEmpController.java
@@ -5,18 +5,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vivek.springbootcrud.model.MongoEmployee;
 import com.vivek.springbootcrud.service.MongoEmpService;
 
 @RestController
+@RequestMapping("/mongo")
 public class MongoEmpController {
-    // @Autowired
-    // private MongoEmpService service;
+    @Autowired
+    private MongoEmpService service;
 
-    // @PostMapping
-    // public ResponseEntity<MongoEmployee> saveOrUpdate(@RequestBody MongoEmployee emp){
-    //     return new ResponseEntity<MongoEmployee>(service.saveOrUpdate(emp),  HttpStatus.ACCEPTED);
-    // }
+    @PostMapping("/save")
+    public ResponseEntity<MongoEmployee> saveOrUpdate(@RequestBody MongoEmployee emp){
+        return new ResponseEntity<MongoEmployee>(service.saveOrUpdate(emp),  HttpStatus.ACCEPTED);
+    }
+
+    
 }
