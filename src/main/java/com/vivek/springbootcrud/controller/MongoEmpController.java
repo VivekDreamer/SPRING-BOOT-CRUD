7ea6c5ad605503dcd8f1f1e7b4d501d9bde27c54
VivekDreamer <vivekmakv@gmail.com>
2023-08-06 21:03:12 +0530

    added an api to get all the employees

--- src/main/java/com/vivek/springbootcrud/controller/MongoEmpController.java
+++ src/main/java/com/vivek/springbootcrud/controller/MongoEmpController.java
@@ -1,8 +1,11 @@
 package com.vivek.springbootcrud.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,5 +25,9 @@ public class MongoEmpController {
         return new ResponseEntity<MongoEmployee>(service.saveOrUpdate(emp),  HttpStatus.ACCEPTED);
     }
 
-    
+    @GetMapping("/all_emp")
+    public ResponseEntity<List<MongoEmployee>> getAllEmployees(){
+        List<MongoEmployee> allEmp = service.getAllEmp();
+        return new ResponseEntity<List<MongoEmployee>>(allEmp,HttpStatus.OK);
+    }
 }
