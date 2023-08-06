6ca88785f529011b9076ee454fa2a1189de952d3
VivekDreamer <vivekmakv@gmail.com>
2023-08-06 21:03:46 +0530

    Small updation in MongoEmpRepo

--- src/main/java/com/vivek/springbootcrud/repository/MongoEmpRepo.java
+++ src/main/java/com/vivek/springbootcrud/repository/MongoEmpRepo.java
@@ -1,10 +1,11 @@
 package com.vivek.springbootcrud.repository;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.vivek.springbootcrud.model.MongoEmployee;
 @Repository
-public interface MongoEmpRepo extends MongoRepository<MongoEmployee,Integer>{
+public interface MongoEmpRepo extends MongoRepository<MongoEmployee,ObjectId>{
     
 }
