7fe03ef9280594cc4ff10b86bbd1915fb346dbd0
VivekDreamer <vivekmakv@gmail.com>
2023-08-07 19:15:10 +0530

    2 new endpoints are added in the MongoEmpController

--- src/main/java/com/vivek/springbootcrud/controller/MongoEmpController.java
+++ src/main/java/com/vivek/springbootcrud/controller/MongoEmpController.java
@@ -2,10 +2,13 @@ package com.vivek.springbootcrud.controller;
 
 import java.util.List;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +32,16 @@ public class MongoEmpController {
     public ResponseEntity<List<MongoEmployee>> getAllEmployees(){
         List<MongoEmployee> allEmp = service.getAllEmp();
         return new ResponseEntity<List<MongoEmployee>>(allEmp,HttpStatus.OK);
+    }
+
+    @GetMapping("/emp/{id}")
+    public ResponseEntity<MongoEmployee> getEmployeeById(@PathVariable ObjectId id){
+        MongoEmployee emp = service.getEmpById(id);
+        return new ResponseEntity<MongoEmployee>(emp,HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteById(@PathVariable ObjectId id){
+        service.deleteById(id);
     }
 }
