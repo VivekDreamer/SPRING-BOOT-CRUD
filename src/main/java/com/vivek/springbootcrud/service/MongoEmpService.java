84b2d1974eef12cf5bca13702db218e0f2ffc174
VivekDreamer <vivekmakv@gmail.com>
2023-08-07 19:15:58 +0530

    2 new methods are added in the MongoEmpService

--- src/main/java/com/vivek/springbootcrud/service/MongoEmpService.java
+++ src/main/java/com/vivek/springbootcrud/service/MongoEmpService.java
@@ -2,10 +2,17 @@ package com.vivek.springbootcrud.service;
 
 import java.util.List;
 
+import org.bson.types.ObjectId;
+
 import com.vivek.springbootcrud.model.MongoEmployee;
 
 public interface MongoEmpService {
     public MongoEmployee saveOrUpdate(MongoEmployee emp);
 
     public List<MongoEmployee> getAllEmp();
+
+    public MongoEmployee getEmpById(ObjectId id);
+
+    public void deleteById(ObjectId id);
+
 }
